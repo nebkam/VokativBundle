a8fc3b5bc4bb0fc74ea790fce87c2359d8489b14
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-06-28 14:43:28 +0200

    Update VokativBundle.php

--- VokativBundle.php
+++ VokativBundle.php
@@ -3,5 +3,17 @@
 namespace NebKam\VokativBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class NebKamVokativBundle extends Bundle {}
+class NebKamVokativBundle extends Bundle
+    {
+    public function build(ContainerBuilder $container)
+        {
+        parent::build($container);
+
+        $extension = new NebKamVokativExtension();
+        $extension->load(array(), $container);
+
+        $container->registerExtension($extension);
+        }
+    } 
