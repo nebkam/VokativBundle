d832ad56a1dbba07df7a2774f6fee1d77f090c7f
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-06-28 14:45:41 +0200

    Update and rename NebKamVokativBundle.php to NebkamVokativBundle.php

--- NebkamVokativBundle.php
+++ NebkamVokativBundle.php
@@ -1,17 +1,17 @@
 <?php
 
-namespace NebKam\VokativBundle;
+namespace Nebkam\VokativBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class NebKamVokativBundle extends Bundle
+class NebkamVokativBundle extends Bundle
     {
     public function build(ContainerBuilder $container)
         {
         parent::build($container);
 
-        $extension = new NebKamVokativExtension();
+        $extension = new NebkamVokativExtension();
         $extension->load(array(), $container);
 
         $container->registerExtension($extension);
