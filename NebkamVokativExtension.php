62c4321a84ac10fbb53687cbbb5c0992c3d2baf3
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-06-28 14:47:56 +0200

    Create NebkamVokativExtension.php

---
+++ NebkamVokativExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Nebkam\Vokativ;
+
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class NebkamVokativExtension extends Extension
+    {
+    public function load(array $configs, ContainerBuilder $container)
+        {
+        }
+    }
