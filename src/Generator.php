d11592d893a070c325bb2a4fad5390d34fed06c5
Nebojša Kamber <kamber@nebojsa.in.rs>
2015-06-28 16:43:22 +0200

    Change to mb_substr

--- src/Generator.php
+++ src/Generator.php
@@ -5,91 +5,93 @@ namespace Nebkam\Vokativ;
 class Generator
 	{
 	/**
-	 * @param string $ime
+	 * @param string $nominativ
 	 * @return string
 	 */
-	public function generate($ime)
+	public function generate($nominativ)
 		{
-		if (substr($ime, -3) == 'tar' || substr($ime, -3) == 'dar') //PETAR, ALEKSANDAR
+		mb_internal_encoding("UTF-8");
+
+		if (mb_substr($nominativ, -3) == 'tar' || mb_substr($nominativ, -3) == 'dar') //PETAR, ALEKSANDAR
 			{
-			$vokativ = substr_replace($ime, 're', -2);
+			$vokativ = substr_replace($nominativ, 're', -2);
 			}
-		elseif(substr($ime, -3) == 'ica' && strlen($ime) > 4)	//MILICA , LJUBICA ALI NE  I MICA i CICA
+		elseif(mb_substr($nominativ, -3) == 'ica' && strlen($nominativ) > 4)	//MILICA , LJUBICA ALI NE  I MICA i CICA
 			{
-			$vokativ = substr_replace($ime, 'ce', -2);
+			$vokativ = substr_replace($nominativ, 'ce', -2);
 			}
-		elseif(substr($ime, -2) == 'ca')	//MACA, CECA...
+		elseif(mb_substr($nominativ, -2) == 'ca')	//MACA, CECA...
 			{
-			$vokativ = substr_replace($ime, 'co', -2);
+			$vokativ = substr_replace($nominativ, 'co', -2);
 			}
-		elseif(substr($ime, -2) == 'sa')	//PERSA, BOSA
+		elseif(mb_substr($nominativ, -2) == 'sa')	//PERSA, BOSA
 			{
-			$vokativ = substr_replace($ime, 'SO', -2);
+			$vokativ = substr_replace($nominativ, 'SO', -2);
 			}
-		elseif(substr($ime, -4) == 'olac')    //KOLAC
+		elseif(mb_substr($nominativ, -4) == 'olac')    //KOLAC
 			{
-			$vokativ = substr_replace($ime, 'oče', -4);
+			$vokativ = substr_replace($nominativ, 'oče', -4);
 			}
-		elseif(substr($ime, -4) == 'alac')    //ZNALAC
+		elseif(mb_substr($nominativ, -4) == 'alac')    //ZNALAC
 			{
-			$vokativ = substr_replace($ime, 'alče', -4);
+			$vokativ = substr_replace($nominativ, 'alče', -4);
 			}
-		elseif(substr($ime, -4) == 'ilac')    //MISLILAC
+		elseif(mb_substr($nominativ, -4) == 'ilac')    //MISLILAC
 			{
-			$vokativ = substr_replace($ime, 'ioče', -4);
+			$vokativ = substr_replace($nominativ, 'ioče', -4);
 			}
-		elseif(substr($ime, -4) == 'elac')    //ŽETELAC
+		elseif(mb_substr($nominativ, -4) == 'elac')    //ŽETELAC
 			{
-			$vokativ = substr_replace($ime, 'eoče', -4);
+			$vokativ = substr_replace($nominativ, 'eoče', -4);
 			}
-		elseif(substr($ime, -3) == 'sac')    //PISAC
+		elseif(mb_substr($nominativ, -3) == 'sac')    //PISAC
 			{
-			$vokativ = substr_replace($ime, 'šče', -3);
+			$vokativ = substr_replace($nominativ, 'šče', -3);
 			}
-		elseif(substr($ime, -3) == 'rac')    //MUDRAC
+		elseif(mb_substr($nominativ, -3) == 'rac')    //MUDRAC
 			{
-			$vokativ = substr_replace($ime, 'rče', -3);
+			$vokativ = substr_replace($nominativ, 'rče', -3);
 			}
-		elseif(substr($ime, -3) == 'vac')    //VALJEVAC
+		elseif(mb_substr($nominativ, -3) == 'vac')    //VALJEVAC
 			{
-			$vokativ = substr_replace($ime, 'vče', -3);
+			$vokativ = substr_replace($nominativ, 'vče', -3);
 			}
-		elseif((substr($ime, -2) == 'ka') && (strlen($ime) > 4))  //DARINKA, MILKA, BORKA, ALI NE I RAKA, MIKA
+		elseif((mb_substr($nominativ, -2) == 'ka') && (strlen($nominativ) > 4))  //DARINKA, MILKA, BORKA, ALI NE I RAKA, MIKA
 			{
-			$vokativ = $ime;
+			$vokativ = $nominativ;
 			}
-		elseif(substr($ime, -2) == 'ja' && substr($ime, -3) != 'ija' && substr($ime, -3) != 'dja' && substr($ime, -3) != 'nja') //MAJA ALI NE I MARIJA, SANJA, NITI PEDJA (NE PEĐA)
+		elseif(mb_substr($nominativ, -2) == 'ja' && mb_substr($nominativ, -3) != 'ija' && mb_substr($nominativ, -3) != 'dja' && mb_substr($nominativ, -3) != 'nja') //MAJA ALI NE I MARIJA, SANJA, NITI PEDJA (NE PEĐA)
 			{
-			$vokativ = substr_replace($ime, 'jo', -2);
+			$vokativ = substr_replace($nominativ, 'jo', -2);
 			}
-		elseif(substr($ime, -2) == 'va' || substr($ime, -2) == 'da' || substr($ime, -2) == 'ba' || substr($ime, -2) == 'ka') //SAVA, DADA, SLOBA, RAKA
+		elseif(mb_substr($nominativ, -2) == 'va' || mb_substr($nominativ, -2) == 'da' || mb_substr($nominativ, -2) == 'ba' || mb_substr($nominativ, -2) == 'ka') //SAVA, DADA, SLOBA, RAKA
 			{
-			$vokativ = substr_replace($ime, 'o', -1);
+			$vokativ = substr_replace($nominativ, 'o', -1);
 			}
-		elseif(substr($ime, -3) == 'ara' || substr($ime, -3) == 'era' || substr($ime, -3) == 'ora') //PERA, DARA, BORA
+		elseif(mb_substr($nominativ, -3) == 'ara' || mb_substr($nominativ, -3) == 'era' || mb_substr($nominativ, -3) == 'ora') //PERA, DARA, BORA
 			{
-			$vokativ = substr_replace($ime, 'ro', -2);
+			$vokativ = substr_replace($nominativ, 'ro', -2);
 			}
-		elseif(substr($ime, -1) == 'k') 	//CUTUK
+		elseif(mb_substr($nominativ, -1) == 'k') 	//CUTUK
 			{
-			$vokativ = substr_replace($ime, 'č', -1).'e';
+			$vokativ = substr_replace($nominativ, 'č', -1).'e';
 			}
-		elseif(substr($ime, -1) == 'g')		//PREDRAG
+		elseif(mb_substr($nominativ, -1) == 'g')		//PREDRAG
 			{
-			$vokativ = substr_replace($ime, 'ž', -1).'e';
+			$vokativ = substr_replace($nominativ, 'ž', -1).'e';
 			}
 		//ć, đ, č, dž, š, ž, lj, nj, j
-		elseif($this->my_substr($ime, -1) == 'ć' || $this->my_substr($ime, -1) == 'đ' || $this->my_substr($ime, -1) == 'č' || $this->my_substr($ime, -2) == 'dž' || $this->my_substr($ime, -1) == 'š' || $this->my_substr($ime, -1) == 'ž' || $this->my_substr($ime, -2) == 'lj' || $this->my_substr($ime, -2) == 'nj' || $this->my_substr($ime, -1) == 'j')
+		elseif(mb_substr($nominativ, -1) == 'ć' || mb_substr($nominativ, -1) == 'đ' || mb_substr($nominativ, -1) == 'č' || mb_substr($nominativ, -2) == 'dž' || mb_substr($nominativ, -1) == 'š' || mb_substr($nominativ, -1) == 'ž' || mb_substr($nominativ, -2) == 'lj' || mb_substr($nominativ, -2) == 'nj' || mb_substr($nominativ, -1) == 'j')
 			{
-			$vokativ = $ime.'u';
+			$vokativ = $nominativ.'u';
 			}
-		elseif(substr($ime, -1) == 'a' || substr($ime, -1) == 'o' || substr($ime, -1) == 'e' || substr($ime, -1) == 'i') //VANJA, MARKO, MILE, MIKI
+		elseif(mb_substr($nominativ, -1) == 'a' || mb_substr($nominativ, -1) == 'o' || mb_substr($nominativ, -1) == 'e' || mb_substr($nominativ, -1) == 'i') //VANJA, MARKO, MILE, MIKI
 			{
-			$vokativ = $ime;
+			$vokativ = $nominativ;
 			}
 		else
 			{
-			$vokativ = $ime.'e';
+			$vokativ = $nominativ.'e';
 			}
 			
 		return $vokativ;
